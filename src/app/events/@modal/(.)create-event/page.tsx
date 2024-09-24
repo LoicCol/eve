a31bae3af4d7d63ebf37698a53455a09667a934b
Loic <loic-col@hotmail.fr>
2024-09-24 20:10:00 +0200

    Little struct change

--- src/app/events/@modal/(.)create-event/page.tsx
+++ src/app/events/@modal/(.)create-event/page.tsx
@@ -1,5 +1,5 @@
-import CreateEventForm from "../../_components/create-event-form";
 import Modal from "@/components/modal";
+import CreateEventForm from "../../_components/create-event-form";
 
 export default function Page() {
   return (
