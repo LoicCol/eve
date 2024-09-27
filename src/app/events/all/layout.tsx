96a806a30eaa464c35ce06b1b9dbe8644ae449a3
Loic <loic-col@hotmail.fr>
2024-09-27 12:57:42 +0200

    remove type

--- src/app/events/all/layout.tsx
+++ src/app/events/all/layout.tsx
@@ -1,13 +1,7 @@
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-  modal: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">All Events</h1>
-
       <div>{children}</div>
     </div>
   );
