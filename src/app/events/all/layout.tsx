cdc738e00e6354fca9e3dc3128554e36fb18b4e4
Loic <loic-col@hotmail.fr>
2024-09-27 12:53:51 +0200

    remove modal

--- src/app/events/all/layout.tsx
+++ src/app/events/all/layout.tsx
@@ -1,6 +1,5 @@
 export default function Layout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
@@ -9,7 +8,6 @@ export default function Layout({
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">All Events</h1>
 
-      <div>{modal}</div>
       <div>{children}</div>
     </div>
   );
