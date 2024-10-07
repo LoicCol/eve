032ca342b1ca66046f238303af724cc1b7d41064
Loic <loic-col@hotmail.fr>
2024-10-07 13:19:12 +0200

    breadcrumb removed on mobile

--- src/components/header/breadcrumb.tsx
+++ src/components/header/breadcrumb.tsx
@@ -1,4 +1,4 @@
-"use server";
+"use client";
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
@@ -11,55 +11,64 @@ import {
 } from "../ui/breadcrumb";
 import { Loader } from "lucide-react";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
-import { getCurrentUser } from "@/server/queries";
 import GroupLink from "./group-link";
 import EventLink from "./event-link";
 import React from "react";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
-export default async function EventBreadcrumb() {
-  const user = await getCurrentUser();
+interface EventBreadcrumbProps {
+  user?: {
+    name?: string;
+    image?: string | null;
+  } | null;
+}
+
+export default function EventBreadcrumb({ user }: EventBreadcrumbProps) {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   return (
-    <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem className="p-0">
-          <BreadcrumbLink asChild>
-            <Link
-              href="/"
-              className="text-xl font-bold text-primary transition-colors duration-300 ease-in-out"
-            >
-              Eve
-            </Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-
-        {user && (
-          <>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem className="flex items-center gap-2">
-              <Avatar className="h-6 w-6">
-                <AvatarImage src={user?.image || ""} alt="user-image" />
-                <AvatarFallback>
-                  <Loader className="h-4 w-4 animate-spin" />
-                </AvatarFallback>
-              </Avatar>
-              <Button
-                variant="link"
-                className="flex items-center p-0 text-foreground after:bg-primary hover:text-primary"
-                asChild
+    isDesktop && (
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem className="p-0">
+            <BreadcrumbLink asChild>
+              <Link
+                href="/"
+                className="text-xl font-bold text-primary transition-colors duration-300 ease-in-out"
               >
-                <Link href="/groups/all" className="capitalize">
-                  {`${user?.name}'s groups`}
-                </Link>
-              </Button>
-            </BreadcrumbItem>
-          </>
-        )}
+                Eve
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
 
-        <GroupLink />
+          {user && (
+            <>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src={user?.image || ""} alt="user-image" />
+                  <AvatarFallback>
+                    <Loader className="h-4 w-4 animate-spin" />
+                  </AvatarFallback>
+                </Avatar>
+                <Button
+                  variant="link"
+                  className="flex items-center p-0 text-foreground after:bg-primary hover:text-primary"
+                  asChild
+                >
+                  <Link href="/groups/all" className="capitalize">
+                    {`${user?.name}'s groups`}
+                  </Link>
+                </Button>
+              </BreadcrumbItem>
+            </>
+          )}
 
-        <EventLink />
-      </BreadcrumbList>
-    </Breadcrumb>
+          <GroupLink />
+
+          <EventLink />
+        </BreadcrumbList>
+      </Breadcrumb>
+    )
   );
 }
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,13 +3,16 @@
 import Link from "next/link";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
+import { getCurrentUser } from "@/server/queries";
 
 import EventBreadcrumb from "./breadcrumb";
 
 export default async function Header() {
+  const user = await getCurrentUser();
+
   return (
     <div className="container mx-auto mb-4 flex h-16 items-center justify-between rounded-lg border px-4">
-      <EventBreadcrumb />
+      <EventBreadcrumb user={user} />
 
       <nav className="flex items-center space-x-4">
         <SignedOut>
