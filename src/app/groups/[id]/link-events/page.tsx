00aa3537452bdbefbbc157df34bc0ef73273e15b
Loic <loic-col@hotmail.fr>
2024-10-13 12:19:32 +0200

    serialize events

--- src/app/groups/[id]/link-events/page.tsx
+++ src/app/groups/[id]/link-events/page.tsx
@@ -9,9 +9,17 @@ export default async function Page({
   params: { id: string };
 }) {
   const events = await getEventsForGroup(decode(id));
+
+  const serializedEvents = events.map((event) => ({
+    eventId: event.eventId,
+    eventName: event.eventName,
+    sectionName: event.sectionName || "Other",
+    sectionId: event.sectionId || "other",
+  }));
+
   return (
     <Modal>
-      <LinkEvents events={events} />
+      <LinkEvents events={serializedEvents} />
     </Modal>
   );
 }
