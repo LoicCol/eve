19ca4f4a79471f504dcb327cefd5441e5c046526
Loic <loic-col@hotmail.fr>
2024-10-15 21:27:41 +0200

    Fixing middleware and events list

--- src/app/events/all/layout.tsx
+++ src/app/events/all/layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container mx-auto pb-2">
-      <h1 className="px-4 text-3xl font-bold">All Events</h1>
+      <h1 className="px-4 text-3xl font-bold">All your groups events</h1>
       <div>{children}</div>
     </div>
   );
