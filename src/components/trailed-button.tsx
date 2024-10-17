c9c38ae9e43fb0927c8dd4f0e70198d929545b44
Loic <loic-col@hotmail.fr>
2024-10-17 15:57:12 +0200

    trailed button border fix

--- src/components/trailed-button.tsx
+++ src/components/trailed-button.tsx
@@ -13,14 +13,14 @@ export function TrailedButton({ children, ...props }: TrailedButtonProps) {
 
   return (
     <div
-      className="relative rounded-md"
+      className="relative rounded-md border border-transparent"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <Button
         variant="outline"
         className={cn(
-          "bg-primary/10 text-primary shadow-none transition-colors duration-300 hover:animate-none hover:bg-primary hover:text-white",
+          "bg-primary/10 text-primary transition-colors duration-300 hover:animate-none hover:bg-primary hover:text-white",
           props.className,
         )}
         {...props}
@@ -29,7 +29,7 @@ export function TrailedButton({ children, ...props }: TrailedButtonProps) {
       </Button>
       <BorderTrail
         size={40}
-        className={`border-2 bg-gradient-to-l from-green-200 via-green-500 to-green-200 dark:from-green-900 dark:via-green-300 dark:to-green-900 ${isHovered ? "hidden" : ""}`}
+        className={`bg-gradient-to-l from-green-200 via-green-500 to-green-200 dark:from-green-900 dark:via-green-300 dark:to-green-900 ${isHovered ? "hidden" : ""}`}
       />
     </div>
   );
