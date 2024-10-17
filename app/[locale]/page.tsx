9ae173c64565e6f7a35246888bf31f142a794ebc
Loic <loic-col@hotmail.fr>
2024-10-17 17:03:09 +0200

    Home page styling

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -9,6 +9,7 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
+import { TextEffect } from "@/components/text-effect";
 
 export default async function Home() {
   const t = await getI18n();
@@ -17,15 +18,21 @@ export default async function Home() {
     <div className="container mx-auto py-8">
       <h1 className="mb-8 px-4 font-sans text-3xl font-bold">
         {t("home.title")}
-        <span className="font-sofia font-sbold ml-4 text-6xl text-primary">
+        <TextEffect
+          preset="blur"
+          as="span"
+          className="font-sofia ml-4 text-6xl text-primary"
+          per="word"
+          delay={0.2}
+        >
           Eve
-        </span>
+        </TextEffect>
       </h1>
       <p className="mb-14 px-4 font-sans text-lg text-foreground">
         {t("home.description")}
       </p>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        <Card className="transition-all duration-300 ease-in-out hover:translate-y-[-3px] hover:rounded-none hover:shadow-lg active:translate-x-[0px] active:translate-y-[0px] active:rounded-xl active:shadow-none">
+        <Card className="border-primary/50 transition-all duration-300 ease-in-out hover:translate-y-[-3px] hover:rounded-none hover:shadow-lg active:translate-x-[0px] active:translate-y-[0px] active:rounded-xl active:shadow-none">
           <CardHeader>
             <CardTitle className="font-sans">
               {t("home.upcomingEvents.title")}
@@ -43,7 +50,7 @@ export default async function Home() {
             </Button>
           </CardFooter>
         </Card>
-        <Card className="transition-all duration-300 ease-in-out hover:translate-y-[-3px] hover:rounded-none hover:shadow-lg active:translate-x-[0px] active:translate-y-[0px] active:rounded-xl active:shadow-none">
+        <Card className="border-primary/50 transition-all duration-300 ease-in-out hover:translate-y-[-3px] hover:rounded-none hover:shadow-lg active:translate-x-[0px] active:translate-y-[0px] active:rounded-xl active:shadow-none">
           <CardHeader>
             <CardTitle className="font-sans">
               {t("home.yourGroups.title")}
