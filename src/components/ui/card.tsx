c04e7865381f25f0e6584e2dc1df51bd8193ac5c
Loic <loic-col@hotmail.fr>
2024-10-22 14:54:57 +0200

    little background fix

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border border-dashed bg-card text-card-foreground shadow-sm",
+      "rounded-lg border border-dashed bg-card/70 text-card-foreground shadow-sm",
       className,
     )}
     {...props}
