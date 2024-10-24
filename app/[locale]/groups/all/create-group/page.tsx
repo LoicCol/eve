4ca122d08b88bc76712b146b522b2da8fd11e31a
Loic <loic-col@hotmail.fr>
2024-10-24 09:25:52 +0200

    usage of layout

--- app/[locale]/groups/all/create-group/page.tsx
+++ app/[locale]/groups/all/create-group/page.tsx
@@ -1,13 +1,5 @@
 import CreateGroupForm from "../create-group-form";
-import CloseButton from "./close-button";
 
 export default function Page() {
-  return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/80">
-      <div className="relative w-full max-w-lg rounded-lg border bg-background p-6 shadow-lg">
-        <CloseButton />
-        <CreateGroupForm />
-      </div>
-    </div>
-  );
+  return <CreateGroupForm />;
 }
