34bcde53158fcdd2032218c8320815a25fbc72e5
Loic <loic-col@hotmail.fr>
2024-10-25 19:05:28 +0200

    local db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from "drizzle-kit";
 
 import { env } from "env";
+import { config } from "dotenv";
+import { neonConfig } from "@neondatabase/serverless";
+
+if (process.env.VERCEL_ENV === "development") {
+  neonConfig.wsProxy = (host) => `${host}:54330/v1`;
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.pipelineTLS = false;
+  neonConfig.pipelineConnect = false;
+}
+
+config({ path: ".env" });
+
+console.log("env", env, process.env.VERCEL_ENV);
 
 export default defineConfig({
   schema: "./server/db/schema.ts",
--- server/db/index.ts
+++ server/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/vercel-postgres";
-import { sql } from "@vercel/postgres";
+// import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
 import * as relations from "./relations";
+import { sql } from "@/db";
 
 export const db = drizzle(sql, { schema: { ...schema, ...relations } });
